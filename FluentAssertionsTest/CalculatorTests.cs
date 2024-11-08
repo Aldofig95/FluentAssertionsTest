c92a1dc2583998aab95cc2678d27aa45bfd2c1e9
aldofwachter <aldo.figueroa@wachter.com>
2024-11-07 18:01:37 -0600

    Did some tutorial test and made one myself

---
+++ FluentAssertionsTest/CalculatorTests.cs
@@ -0,0 +1,86 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace FluentAssertionsTest
+{
+    public class CalculatorTests
+    {
+        public Calculator _calculator;
+
+        public CalculatorTests()
+        {
+            _calculator = new Calculator();
+        }
+
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var a = 5;
+            var b = 3;
+
+            var result = _calculator.Add(a, b);
+
+            result.Should().Be(8);
+        }
+
+        [Fact]
+        public void Subtract_ShouldReturnCorrectDifference()
+        {
+            var a = 10;
+            var b = 4;
+
+            var result = _calculator.Subtract(a, b);
+
+            result.Should().Be(6);
+        }
+
+        [Fact]
+        public void Multiply_ShouldReturnCorrectProduct()
+        {
+            var a = 6;
+            var b = 7;
+
+            var result = _calculator.Multiply(a, b);
+
+            result.Should().Be(42);
+        }
+
+        [Fact]
+        public void Divide_ShouldReturnCorrectQuotient()
+        {
+            var a = 20;
+            var b = 5;
+
+            var result = _calculator.Divide(a, b);
+
+            result.Should().Be(4);
+        }
+
+        [Fact]
+        public void Divide_ShouldThrowDivideByZeroException()
+        {
+            var a = 10;
+            var b = 0;
+
+            Action act = () => _calculator.Divide(a, b);
+
+            act.Should().Throw<DivideByZeroException>();
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public int Divide(int a, int b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+    }
+}
+
--- FluentAssertionsTest/FluentAssertionsTutorial.cs
+++ FluentAssertionsTest/FluentAssertionsTutorial.cs
@@ -1,11 +1,44 @@
+using FluentAssertions;
+
 namespace FluentAssertionsTest
 {
     public class FluentAssertionsTutorial
     {
         [Fact]
-        public void Test1()
+        public void Tutorial()
         {
-            Assert.True(true);
+            string actual = "ABCDEFGHI";
+            var result = actual.Should().StartWith("AB").And.EndWith("HI").And.Contain("EF").And.HaveLength(9);
         }
+
+        [Fact]
+        public void Tutorial2()
+        {
+            IEnumerable<int> numbers = new[] { 1, 2, 3 };
+
+            numbers.Should().OnlyContain(n => n > 0);
+            numbers.Should().HaveCount(4, "because we thought we put four items in the collection");
+        }
+
+        [Fact]
+        public void Tutorial3()
+        {
+            string username = "dennis";
+            username.Should().Be("jonas");
+        }
+
+        [Fact]
+        public void Tutorial4()
+        {
+            object theObject = null;
+
+            theObject.Should().BeNull("because the value is null");
+            theObject.Should().NotBeNull();
+
+            theObject = "whatever";
+            theObject.Should().BeOfType<string>("because a {0} is set", typeof(string));
+            theObject.Should().BeOfType(typeof(string), "because a {0} is set", typeof(string));
+        }
+            
     }
 }
